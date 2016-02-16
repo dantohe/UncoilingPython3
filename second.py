ef9bc2628b41e3947d2c6a025d65f22f5a8bcc8f
daniel antohe <dantohe@gmail.com>
2016-02-16 10:19:33 -0700

    add stubs

---
+++ second.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 16, 2016
+
+@author: daniel
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ third.py
@@ -0,0 +1,11 @@
+'''
+Created on Feb 16, 2016
+
+@author: daniel
+'''
+import sys
+def main():
+    print ('Hello there', sys.argv[0])
+    
+if __name__ == '__main__':
+    main()
