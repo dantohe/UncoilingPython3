06c85b02b04fbc0412d9e5dc07ca72a9ad18e0c4
daniel antohe <dantohe@gmail.com>
2016-02-17 08:58:39 -0700

    exerc changes

--- third.py
+++ third.py
@@ -7,6 +7,9 @@ import sys
 def main():
     print ('Hello there', sys.argv[0])
     print ("yet another")
+    myString='this is my string'
+    print(myString[0])
+    print(len(myString))
     
 if __name__ == '__main__':
     main()
